79613b320402a57f9fa38f800b83e0812ea66a07
jenspt <jens.perregaard.thorsen@gmail.com>
2023-01-03 20:00:35 +0100

    created template w cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Mandatory project for 02476 MLOps course at DTU. Project scope to be determined.',
+    author='Spyros-Benjamin-Jens',
+    license='',
+)
