ddbd1539bb640b2a698c690e004aaed641e0afac
jenspt <jens.perregaard.thorsen@gmail.com>
2023-01-09 15:22:51 +0100

    black and flake8 working together

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -10,8 +10,8 @@ from src.models.dataset import MyDataset
 from src.models.model import MyAwesomeModel
 
 
-@click.command(context_settings={'show_default': True})
-@click.argument('train_set', type=click.Path(exists=True))
+@click.command(context_settings={"show_default": True})
+@click.argument("train_set", type=click.Path(exists=True))
 @click.option("--lr", default=1e-3)
 @click.option("--bs", default=128)
 @click.option("--shuffle", is_flag=True)
@@ -26,7 +26,7 @@ def main(train_set, lr, bs, shuffle, workers, epochs, wandb_log):
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    logger.info('initializing dataset')
+    logger.info("initializing dataset")
     dataset = MyDataset(train_set)
     dataloader = torch.utils.data.DataLoader(
         dataset,
@@ -35,7 +35,7 @@ def main(train_set, lr, bs, shuffle, workers, epochs, wandb_log):
         num_workers=workers,
     )
 
-    logger.info('initializing model')
+    logger.info("initializing model")
     model = MyAwesomeModel().to(device)
 
     if wandb_log:
@@ -44,7 +44,7 @@ def main(train_set, lr, bs, shuffle, workers, epochs, wandb_log):
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     criterion = torch.nn.CrossEntropyLoss()
 
-    logger.info('starting training loop')
+    logger.info("starting training loop")
     loss_curve = []
     for epoch in range(epochs):
         total_loss = 0
@@ -57,25 +57,24 @@ def main(train_set, lr, bs, shuffle, workers, epochs, wandb_log):
             total_loss += loss.item()
             if wandb_log:
                 wandb.log({"loss": loss})
-        avg_loss = total_loss/len(dataloader)
+        avg_loss = total_loss / len(dataloader)
         loss_curve.append(avg_loss)
-        logger.info(f'Epoch {epoch}; loss: {avg_loss:.4f}')
+        logger.info(f"Epoch {epoch}; loss: {avg_loss:.4f}")
 
-    outdir = 'models'
+    outdir = "models"
     if not os.path.exists(outdir):
         os.makedirs(outdir)
-    outfile = 'trained_model.pt'
+    outfile = "trained_model.pt"
     torch.save(model.state_dict(), os.path.join(outdir, outfile))
 
     plt.plot(loss_curve)
-    plt.xlabel('Epoch')
-    plt.ylabel('Loss')
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
     plotfile = "training_curve.png"
     plt.savefig(os.path.join(outdir, plotfile))
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+if __name__ == "__main__":
+    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     logging.basicConfig(level=logging.INFO, format=log_fmt)
     main()
-
