249e81c1f2762a29ea2c4d7a61f14e14cd59b1d7
Spyros Vlachospyros <spiros.vlachospiros@gmail.com>
2023-01-09 12:34:22 +0100

    i

---
+++ tests/__init__.py
@@ -0,0 +1,4 @@
+import os
+_TEST_ROOT = os.path.dirname('tests')  # root of test folder
+_PROJECT_ROOT = os.path.dirname('02476-mlops')  # root of project
+_PATH_DATA = os.path.join('data', "Data")  # root of data
