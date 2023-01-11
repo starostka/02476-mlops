fa692d3b643f0226b8a8b5b5124ff7e5bbe6faa4
jenspt <jens.perregaard.thorsen@gmail.com>
2023-01-11 18:55:13 +0100

    adapt to Cora dataset. Model, train and predict

--- src/models/model.py
+++ src/models/model.py
@@ -1,26 +1,21 @@
-from torch import nn
+from torch_geometric.nn import GCNConv
+import torch.nn.functional as F
+import torch
 
 
-class MyAwesomeModel(nn.Module):
-    def __init__(self):
+class GCN(torch.nn.Module):
+    def __init__(self, hidden_channels):
         super().__init__()
-        self.backbone = nn.Sequential(
-            nn.Conv2d(1, 64, 3),
-            nn.LeakyReLU(),
-            nn.Conv2d(64, 32, 3),
-            nn.LeakyReLU(),
-            nn.Conv2d(32, 16, 3),
-            nn.LeakyReLU(),
-            nn.Conv2d(16, 8, 3),
-            nn.LeakyReLU()
-        )
+        torch.manual_seed(42)
+        self.conv1 = GCNConv(1433, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, 7)
 
-        self.classifier = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(8 * 20 * 20, 128),
-            nn.Dropout(),
-            nn.Linear(128, 10)
-        )
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index)
+        x = x.relu()
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = self.conv2(x, edge_index)
+        return x
 
-    def forward(self, x):
-        return self.classifier(self.backbone(x))
+
+model = GCN(hidden_channels=16)
--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -2,50 +2,39 @@ import logging
 
 import click
 import torch
-import wandb
-
-from src.models.dataset import MyDataset
-from src.models.model import MyAwesomeModel
+from src.models.model import GCN
 
 
 @click.command()
-@click.argument('model_checkpoint', type=click.Path(exists=True))
-@click.argument('test_set', type=click.Path(exists=True))
+@click.argument("model_checkpoint", type=click.Path(exists=True))
 @click.option("--wandb", "wandb_log", is_flag=True)
-def main(model_checkpoint, test_set, wandb_log):
+def main(model_checkpoint, wandb_log):
     logger = logging.getLogger(__name__)
 
-    if wandb_log:
-        wandb.init()
-
-    logger.info('loading model')
-    model = MyAwesomeModel()
+    logger.info("loading model")
+    model = GCN(hidden_channels=16)
     state = torch.load(model_checkpoint)
     model.load_state_dict(state)
     model.eval()
 
-    logger.info('loading data')
-    dataset = MyDataset(test_set)
-    input_, target = dataset[:]
+    logger.info("loading data")
+    data = torch.load("data/processed/data.pt")[0]  # access first and only graph
 
-    logger.info('predicting')
-    output = model(input_).argmax(dim=-1)
-    acc = torch.sum(output == target)/len(output)
+    logger.info("predicting")
+    out = model(data.x, data.edge_index)
+    pred = out.argmax(dim=1)  # Use the class with highest probability.
+    test_correct = (
+        pred[data.test_mask] == data.y[data.test_mask]
+    )  # Check against ground-truth labels.
+    test_acc = int(test_correct.sum()) / int(
+        data.test_mask.sum()
+    )  # Derive ratio of correct predictions.
 
-    logger.info(f'accuracy: {acc:.2f}')
-
-    if wandb_log:
-        columns = ["id", "image", "guess", "truth"]
-        data = [
-            [i, wandb.Image(input_[i]), output[i], target[i]]
-            for i in range(len(dataset))
-        ]
-        table = wandb.Table(data=data, columns=columns)
-        wandb.log({'prediction_table': table})
+    logger.info(f"accuracy: {test_acc:.2f}")
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+if __name__ == "__main__":
+    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
     main()
