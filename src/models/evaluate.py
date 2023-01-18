0f09ccc3755659c92a5829d4dd6a661440c7b465
philgzl <services@philgzl.com>
2023-01-18 12:18:23 +0100

    Refactor training and evaluation with Lightning (#39)
    * Add dataset path as config value and CLI argument for all scripts
    
    * Remove DVC and add conf dir to dockerfile
    
    * Add Vertex job config
    
    * Add new config file
    
    * Implement Lightning
    
    * Update README.md
    
    * Add new renamed scripts
    
    * Fix .gitignore ignoring src/models
    
    * Add venv, lightning logs and hydra outputs
    
    * Add Lightning to requirements
    
    * Restore models.dvc
    
    * Restore new line at end of models.dvc

--- src/models/evaluate.py
+++ src/models/evaluate.py
@@ -9,33 +9,26 @@ from src.models.model import GCN
 @hydra.main(version_base=None, config_path="../../conf", config_name="config")
 def main(cfg: DictConfig) -> None:
     # wandb_log = cfg.wandb
-    model_checkpoint = cfg.checkpoint
     logger = logging.getLogger(__name__)
 
     logger.info("loading model")
-    model = GCN(hidden_channels=16)
-    state = torch.load(model_checkpoint)
-    model.load_state_dict(state)
-    model.eval()
+    model = GCN(
+        hidden_channels=cfg.hyperparameters.hidden_channels,
+        learning_rate=cfg.hyperparameters.learning_rate,
+        weight_decay=cfg.hyperparameters.weight_decay,
+    )
+    state = torch.load(cfg.checkpoint)
+    model.load_state_dict(state["state_dict"])
 
     logger.info("loading data")
     data = torch.load(cfg.dataset)[0]  # access first and only graph
 
-    logger.info("predicting")
-    out = model(data.x, data.edge_index)
-    pred = out.argmax(dim=1)  # Use the class with highest probability.
-    test_correct = (
-        pred[data.test_mask] == data.y[data.test_mask]
-    )  # Check against ground-truth labels.
-    test_acc = int(test_correct.sum()) / int(
-        data.test_mask.sum()
-    )  # Derive ratio of correct predictions.
-
-    logger.info(f"accuracy: {test_acc:.2f}")
+    logger.info("evaluating")
+    acc = model.evaluate(data)
+    logger.info(f"accuracy: {acc:.2f}")
 
 
 if __name__ == "__main__":
     log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     logging.basicConfig(level=logging.INFO, format=log_fmt)
-
     main()
--- tests/test_model.py
+++ tests/test_model.py
@@ -2,19 +2,42 @@ import torch
 import pytest
 import os
 
-# from src.models.dataset import MyDataset
+import omegaconf
+
 from src.models.model import GCN
-from tests import _PATH_DATA
+from tests import _PATH_DATA, _PATH_CONF
+
+
+def _init_dataset_and_model():
+    data = torch.load(_PATH_DATA)[0]
+
+    cfg = omegaconf.OmegaConf.load(_PATH_CONF)
+    model = GCN(
+        hidden_channels=cfg.hyperparameters.hidden_channels,
+        learning_rate=cfg.hyperparameters.learning_rate,
+        weight_decay=cfg.hyperparameters.weight_decay,
+    )
+    model.eval()
+
+    return data, model
 
 
 @pytest.mark.skipif(not os.path.exists(_PATH_DATA), reason="Data files not found")
 def test_model_output_shape():
-    dataset = torch.load(_PATH_DATA)
-    data = dataset[0]
-    model = GCN(hidden_channels=16)
-    model.eval()
-
+    data, model = _init_dataset_and_model()
     out = model(data.x, data.edge_index)
-    pred = out.argmax(dim=1)  # Use the class with highest probability.
+    assert out.shape == torch.Size([2708, 7])  # predictions for all 2708 nodes
 
-    assert pred.shape == torch.Size([2708])  # predictions for all 2708 nodes
+
+@pytest.mark.skipif(not os.path.exists(_PATH_DATA), reason="Data files not found")
+def test_model_evaluate():
+    data, model = _init_dataset_and_model()
+    acc = model.evaluate(data)
+    assert isinstance(acc, float)
+
+
+@pytest.mark.skipif(not os.path.exists(_PATH_DATA), reason="Data files not found")
+def test_model_predict():
+    data, model = _init_dataset_and_model()
+    pred = model.predict(data, 0)
+    assert isinstance(pred, str)
